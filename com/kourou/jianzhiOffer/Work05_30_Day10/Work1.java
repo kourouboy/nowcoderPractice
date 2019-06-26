8de6a0a4a5abb10483b0f9eec9b30e456a448a09
kourouboy <1376483126@qq.com>
2019-06-26 20:12:00 +0800

    .

--- com/kourou/jianzhiOffer/Work05_30_Day10/Work1.java
+++ com/kourou/jianzhiOffer/Work05_30_Day10/Work1.java
@@ -8,6 +8,18 @@ import java.util.Scanner;
 public class Work1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        int n = 0;
+        int a = 0,b = 1,c = 1;
+        if (in.hasNextInt()){
+            n = in.nextInt();
+        }
+        while (c < n){
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        int x = n - b;
+        int y = c - n;
+        System.out.println(x<y?x:y);
     }
 }
