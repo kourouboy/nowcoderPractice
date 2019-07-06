4703d380ae0bbc1a06d5499c2f001d0fc6dcd852
kourouboy <1376483126@qq.com>
2019-07-06 22:32:11 +0800

    .

---
+++ com/kourou/jianzhiOffer/Work7_4_Day28/Work1.java
@@ -0,0 +1,27 @@
+package com.kourou.jianzhiOffer.Work7_4_Day28;
+
+import java.util.Scanner;
+
+/**
+ * 守形数
+ */
+public class Work1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = n * n;
+        while (in.hasNext()) {
+            if (n < 10) {
+                if (m % 10 == n)
+                    System.out.println("YES!");
+                else
+                    System.out.println("NO!");
+            } else {
+                if (m % 100 == n)
+                    System.out.println("YES!");
+                else
+                    System.out.println("NO!");
+            }
+        }
+    }
+}
