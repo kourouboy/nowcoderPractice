f0d4e78917c93535f1f2c09729c5f96aa7f637de
kourouya <1376483126@qq.com>
2019-07-23 23:49:58 +0800

    .

---
+++ com/kourou/jianzhiOffer/Work7_22_Day43/Work1.java
@@ -0,0 +1,17 @@
+package com.kourou.jianzhiOffer.Work7_22_Day43;
+
+import java.util.Scanner;
+
+/**
+ * 电话号码
+ */
+public class Work1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()){
+            String str = in.nextLine();
+            char[] num = str.toCharArray();
+            
+        }
+    }
+}
