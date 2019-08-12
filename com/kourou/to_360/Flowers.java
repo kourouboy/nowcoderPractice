073250481448ba2609ee16d17695692bc5b67cc1
kourouya <1376483126@qq.com>
2019-08-12 23:31:48 +0800

    .

--- com/kourou/to_360/Flowers.java
+++ com/kourou/to_360/Flowers.java
@@ -5,8 +5,16 @@ package com.kourou.to_360;
  * @Data： 2019/8/11 21:08
  * @Description：
  */
-public class Flowers {
-    public static void main(String[] args) {
-        
+public class Flowers{
+    public static Test t1=new Test();
+    {
+        System.out.println("blockA");
+    }
+    static{
+        System.out.println("blockB");
+    }
+    public static void main(String[] args)
+    {
+        Test t2=new Test();
     }
 }
